065a9f4c9e269cd23bdb36f02181e7563b0d49fe
Nirmal Mekala <ndmekala@icloud.com>
2025-07-25 07:38:24 -0500

    configure in the lazy way instead of using after

--- nvim/after/plugin/telescope.lua
+++
@@ -1,4 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>3', builtin.live_grep)
--- TODO ID if this needs to be git_files
-vim.keymap.set('n', '<leader>4', builtin.find_files)
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  cmd = "Telescope",
+  keys = {
+    { "<leader>3", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+    -- TODO identify if this shoudl be git files
+    { "<leader>4", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+  },
   config = function()
     require('telescope').setup{
       defaults = {
@@ -12,6 +18,7 @@ return {
           }
         }
       }}
+
     end
   }
 
