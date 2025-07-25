57608a101e228bf67635c6ea5021b9b0a9fa8aba
Nirmal Mekala <ndmekala@icloud.com>
2025-07-25 07:43:12 -0500

    toggle term the lazy nvim way

--- nvim/after/plugin/toggleterm.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set('n', '<leader>2', ':ToggleTerm<CR>', { noremap = true })
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,5 +1,11 @@
 return {
-  'akinsho/toggleterm.nvim', version = "*", config = function()
+  'akinsho/toggleterm.nvim', 
+  version = "*", 
+  keys = {
+    { "<leader>2", "<cmd>ToggleTerm <cr>", mode = "n", desc = "Toggle Terminal" },
+  },
+
+  config = function()
     require('toggleterm').setup{
       direction = 'float',
     }
