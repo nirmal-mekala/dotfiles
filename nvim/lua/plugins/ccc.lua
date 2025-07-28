37f79fa068ea95dca71ff13b4698be2492055022
Nirmal Mekala <ndmekala@icloud.com>
2025-07-28 14:29:41 -0500

    use ccc instead of nvim colorizer

---
+++ nvim/lua/plugins/ccc.lua
@@ -0,0 +1,16 @@
+return {
+	"uga-rosa/ccc.nvim",
+	config = function()
+		local ccc = require("ccc")
+		local mapping = ccc.mapping
+
+		ccc.setup({
+			-- Your preferred settings
+			-- Example: enable highlighter
+			highlighter = {
+				auto_enable = true,
+				lsp = false,
+			},
+		})
+	end,
+}
