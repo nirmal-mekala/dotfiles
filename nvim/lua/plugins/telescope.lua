d9fb0944566a93f6c369846e20ddf9e672f23fc7
Nirmal Mekala <ndmekala@icloud.com>
2025-07-29 11:43:43 -0500

    smart new tab

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,24 +1,52 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  cmd = "Telescope",
-  keys = {
-    { "<leader>3", "<cmd>Telescope live_grep<cr>", mode = "n", desc = "Live Grep" },
-    -- TODO identify if this shoudl be git files
-    { "<leader>4", "<cmd>Telescope find_files<cr>", mode = "n", desc = "Find Files" },
-  },
-  config = function()
-    require('telescope').setup{
-      defaults = {
-        layout_strategy = 'vertical',
-        layout_config = { height = 0.95 },
-        mappings = {
-          i = {
-            ['<CR>'] = require('telescope.actions').select_tab,
-          }
-        }
-      }}
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = "Telescope",
+	keys = {
+		{ "<leader>3", "<cmd>Telescope live_grep<cr>", mode = "n", desc = "Live Grep" },
+		-- TODO identify if this shoudl be git files
+		{ "<leader>4", "<cmd>Telescope find_files<cr>", mode = "n", desc = "Find Files" },
+	},
+	config = function()
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
 
-    end
-  }
+		local function smart_tab_open(prompt_bufnr)
+			local entry = action_state.get_selected_entry()
+			local filepath = entry.path or entry.filename
+			actions.close(prompt_bufnr)
 
+			-- Check for existing tab with the file
+			for tabnr = 1, vim.fn.tabpagenr("$") do
+				for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tabnr)) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					local name = vim.api.nvim_buf_get_name(buf)
+					if name == vim.fn.fnamemodify(filepath, ":p") then
+						vim.cmd(tabnr .. "tabnext")
+						vim.api.nvim_set_current_win(win)
+						return
+					end
+				end
+			end
+
+			-- If not found, open in new tab
+			vim.cmd("tabnew " .. vim.fn.fnameescape(filepath))
+		end
+
+		require("telescope").setup({
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = { height = 0.95 },
+				mappings = {
+					i = {
+						["<CR>"] = smart_tab_open,
+					},
+					n = {
+						["<CR>"] = smart_tab_open,
+					},
+				},
+			},
+		})
+	end,
+}
