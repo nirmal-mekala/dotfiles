df4dc0b216f57d6fbf8c401d864f8deaa6d5707c
Nirmal Mekala <ndmekala@icloud.com>
2025-07-30 22:54:21 -0500

    toggle term create new terminals and select between them

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,5 @@
 return {
 	"akinsho/toggleterm.nvim",
-	version = "*",
 	keys = {
 		{
 			"<C-j>",
@@ -13,6 +12,18 @@ return {
 			"<C-\\><C-n><cmd>ToggleTerm<cr>",
 			mode = "t",
 			desc = "Exit terminal mode + toggle term",
+		},
+		{
+			"<leader>n",
+			"<cmd>TermNew<cr>",
+			mode = { "n", "t" },
+			desc = "New Terminal",
+		},
+		{
+			"<leader>s",
+			"<cmd>TermSelect<cr>",
+			mode = { "n", "t" },
+			desc = "Select Terminal",
 		},
 	},
 
