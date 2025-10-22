21b43ac5ec688fe3ebe12e38441a657d68bd7f1c
Nirmal Mekala <ndmekala@icloud.com>
2025-10-22 09:48:03 -0500

    env configurable ai

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,15 +1,36 @@
--- TODO make this dynamic based on a gitignored config file ('off' | 'codeium' | 'copilot')
+local ok, ai_config = pcall(require, "config.ai")
 
-return {
-	"Exafunction/windsurf.vim",
-	enabled = false,
-	event = "BufEnter",
-	config = function()
-		vim.g.codeium_filetypes = {
-			markdown = false,
-			text = false,
-			csv = false,
-			json = false,
-		}
-	end,
-}
+local provider = "none"
+if ok and ai_config and ai_config.ai_provider then
+	provider = ai_config.ai_provider
+end
+
+if provider == "copilot" then
+	return {
+		"github/copilot.vim",
+		event = "InsertEnter",
+		--		config = function()
+		--			vim.cmd([[ let g:copilot_no_tab_map = v:true ]])
+		--			vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', {
+		--				expr = true,
+		--				silent = true,
+		--				noremap = true,
+		--			})
+		--		end,
+	}
+elseif provider == "codeium" then
+	return {
+		"Exafunction/windsurf.vim",
+		event = "BufEnter",
+		config = function()
+			vim.g.codeium_filetypes = {
+				markdown = false,
+				text = false,
+				csv = false,
+				json = false,
+			}
+		end,
+	}
+else
+	return {}
+end
