00252f185d3945e18ae51d8ba063d94b59b8ffee
Nirmal Mekala <ndmekala@icloud.com>
2025-11-06 12:40:11 -0600

    optionally gd in same buffer

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,31 +1,41 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
+		local function goto_definition_new_tab()
+			local params = vim.lsp.util.make_position_params()
+
+			-- Request definition locations
+			vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx, _)
+				if err or not result or vim.tbl_isempty(result) then
+					vim.notify("No definition found", vim.log.levels.WARN)
+					return
+				end
+
+				-- Handle both Location and Location[] responses
+				local location = result[1] or result
+				local uri = location.uri or location.targetUri
+				local range = location.range or location.targetSelectionRange
+				local target_bufname = vim.uri_to_fname(uri)
+				local current_bufname = vim.api.nvim_buf_get_name(0)
+
+				if target_bufname ~= current_bufname then
+					vim.cmd("tab split")
+				else
+				end
+				vim.lsp.buf.definition({ reuse_win = true })
+
+				--vim.lsp.util.jump_to_location(location, "utf-8")
+			end)
+		end
+
 		local on_attach = function(client, bufnr)
 			vim.keymap.set("n", "gd", function()
-				local params = vim.lsp.util.make_position_params()
-
-				-- Request definition locations
-				vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx, _)
-					if err or not result or vim.tbl_isempty(result) then
-						vim.notify("No definition found", vim.log.levels.WARN)
-						return
-					end
-
-					-- Handle both Location and Location[] responses
-					local location = result[1] or result
-					local uri = location.uri or location.targetUri
-					local range = location.range or location.targetSelectionRange
-					local target_bufname = vim.uri_to_fname(uri)
-					local current_bufname = vim.api.nvim_buf_get_name(0)
-
-					if target_bufname ~= current_bufname then
-						vim.cmd("tab split")
-					end
-
-					vim.lsp.util.jump_to_location(location, "utf-8")
-				end)
+				goto_definition_new_tab()
 			end, { buffer = bufnr, desc = "Goto Definition (new tab if different file)" })
+
+			vim.keymap.set("n", "<leader>gd", function()
+				vim.lsp.buf.definition({ reuse_win = true })
+			end, { buffer = bufnr, desc = "Goto Definition (same tab)" })
 
 			vim.keymap.set("n", "H", function()
 				vim.lsp.buf.hover()
