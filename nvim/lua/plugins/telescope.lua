d3b6398ad6fc2668cf3e6544e8b9ad487201318c
Nirmal Mekala <ndmekala@icloud.com>
2025-11-21 19:46:01 -0600

    live grep now searches all git files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,7 +15,26 @@ return {
 		{
 			"<leader>3",
 			function()
-				require("telescope.builtin").live_grep()
+      	local function is_git_repo()
+      		vim.fn.system("git rev-parse --is-inside-work-tree")
+      
+      		return vim.v.shell_error == 0
+      	end
+      
+      	local function get_git_root()
+      		local dot_git_path = vim.fn.finddir(".git", ".;")
+      		return vim.fn.fnamemodify(dot_git_path, ":h")
+      	end
+      
+      	local opts = {}
+      
+      	if is_git_repo() then
+      		opts = {
+      			cwd = get_git_root(),
+      		}
+      	end
+      
+      	require("telescope.builtin").live_grep(opts)
 			end,
 			mode = "n",
 			desc = "Live Grep",
